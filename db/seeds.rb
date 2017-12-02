7610f9c1534dcf0a057de6ffd270763107cd98cb
kaushalkishorersmt <kaushalkishorersmt@gmail.com>
2017-12-02 19:14:27 +0530

    Add rack-cors to access the API.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |d|
+MicroserviceDocument.create!(
+  title: "Document #{d}",
+  description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+  file_url: "https://docs.google.com/document/d/1y0RWGxLuZy3rveWeCQVGtAZrQhQnfHWT7l5J6trL3Y8/edit?usp=sharing",
+  image_url: "https://upload.wikimedia.org/wikipedia/commons/2/20/Services4.png"
+)
+end
